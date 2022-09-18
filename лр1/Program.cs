19d5d2e69a09cb26fd23ea30d99ec04c4dcae54e
ira ostanina <irina.rus.ostanina@gmail.com>
2022-09-18 12:31:45 +0300

    Добавьте файлы проекта.

---
+++ лр1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace лр1
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("byte:\n min: {byte.MinValue}\n max: {byte.MaxValue} \n" +
+                           "sbyte:\n min: {sbyte.MinValue}\n max: {sbyte.MaxValue} \n" +
+                           "short:\n min: {short.MinValue}\n max: {short.MaxValue} \n" +
+                           "ushort:\n min: {ushort.MinValue}\n max: {ushort.MaxValue} \n" +
+                           "int:\n min: {int.MinValue}\n max: {int.MaxValue} \n" +
+                           "uint:\n min: {uint.MinValue}\n max: {uint.MaxValue} \n" +
+                           "long:\n min: {long.MinValue}\n max: {long.MaxValue} \n" +
+                           "ulong:\n min: {ulong.MinValue}\n max: {ulong.MaxValue} \n" +
+                           "double:\n min: {double.MinValue}\n max: {double.MaxValue} \n" +
+                           "float:\n min: {float.MinValue}\n max: {float.MaxValue} \n");
+
+        }
+
+    }
+}
+
+
